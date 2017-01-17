5224c4acf5817c286ff3a1eefa7527335a6cb635
Michal <r.pr.michal.doniec@gmail.com>
2017-01-17 23:43:44 +0100

    Added submission class, amendments to init,  save and loading methods in student class.

--- student.py
+++ student.py
@@ -7,17 +7,18 @@ class Student(User):
 
     ALL_STUDENTS = []
 
-    def __init__(self,name, surname, age, gender, pesel, login, password, date_removed=None, status= 'Active'):
+    def __init__(self,name, surname, age, gender, pesel, login, password, date_removed=None, status= 'Active',submissions= []):
         User.__init__(self,name, surname, age, gender, pesel, login, password, date_removed, status)
         self.class_attendance = Attendance.create_attendance()
         self.list_of_attendance = self.class_attendance
-        self.submissions = []
-        self.grades = []
+        self.submissions = submissions
         self.ALL_STUDENTS.append(self)
 
-    def create_submission(self, link, description):
+    def submit_submission(self, link, description,assignment):
 
-        self.submissions.append(Submission(link,description))
+        self.submissions = Submission(link,description,assignment)
+
+        self.submissions.add_submission_to_submissions_list()
 
     def view_submissions(self):
 
@@ -25,18 +26,36 @@ class Student(User):
 
     def __str__(self):
 
-        return 'Student: {} {}, age: {}, gender: {}, PESEL: {}, Status: {}, Grades: {}'.format(self.name, self.surname, self.age, self.gender, self.pesel, self.status, self.grades)
+        grades = ''
+        if self.submissions:
+            for submission in self.submissions.submissions_list:
+                for grade in range(len(submission.grades)):
+                    if grade == len(submission.grades)-1:
+
+                        grades += str(submission.grades[grade])
+
+                    else:
+                        grades += str(submission.grades[grade]) + ','
+
+        return 'Student: {} {}, age: {}, gender: {}, PESEL: {}, Status: {}, Grades: {}.'.format(self.name, self.surname, self.age, self.gender, self.pesel, self.status, grades)
 
     def to_list(self):
 
         attendance_dates_status = ''
 
         for attendance in self.list_of_attendance.attendance_list:
-            print(attendance.date)
-            print(attendance.status)
             attendance_dates_status += attendance.date + ':' + str(attendance.status) + '|'
 
-        return [self.name, self.surname, str(self.age), self.gender, self.pesel, self.login, self._password, str(self.date_removed), self.status, self.date_added, attendance_dates_status]
+
+        submissions = []
+
+
+        if self.submissions:
+            for submission in self.submissions.submissions_list:
+                submission_data = [submission.link, submission.description, str(submission.grades)]
+                submissions.append(submission_data)
+
+        return [self.name, self.surname, str(self.age), self.gender, self.pesel, self.login, self._password, str(self.date_removed), self.status, str(self.date_added), str(attendance_dates_status), str(submissions)]
 
 
     @classmethod
@@ -49,33 +68,46 @@ class Student(User):
                 line_splitted = line.strip('\n').split(',')
 
                 try:
+                    print(line_splitted[11],line_splitted[12], line_splitted[13])
 
+
+                    one_from_students = Student(line_splitted[0], line_splitted[1], int(line_splitted[2]),
+                                                line_splitted[3],
+                                                line_splitted[4], line_splitted[5], line_splitted[6], line_splitted[7],
+                                                line_splitted[8])
+
+                    one_from_students.submit_submission(line_splitted[11],line_splitted[12], line_splitted[13])
+
+                except IndexError:
+                    one_from_students = Student(line_splitted[0], line_splitted[1], int(line_splitted[2]),
+                                                line_splitted[3],
+                                                line_splitted[4], line_splitted[5], line_splitted[6], line_splitted[7],
+                                                line_splitted[8])
+                try:
                     attendance_dates_status = line_splitted[10].split('|')
 
                     separated_dates_status = []
                     for item in attendance_dates_status:
                         separated_dates_status.append(item.split(':'))
 
-                    one_from_students = Student(line_splitted[0],line_splitted[1],int(line_splitted[2]), line_splitted[3],line_splitted[4],line_splitted[5],line_splitted[6],line_splitted[7],line_splitted[8])
 
-
-                    print(separated_dates_status)
                     for element in range(len(separated_dates_status)):
 
                         one_from_students.list_of_attendance.checking_presence(separated_dates_status[element][0], int(separated_dates_status[element][1]))
 
                 except IndexError:
-                    Student(line_splitted[0], line_splitted[1], int(line_splitted[2]), line_splitted[3], line_splitted[4], line_splitted[5], line_splitted[6], line_splitted[7], line_splitted[8])
+                    pass
 
             class_file.close()
 
     @classmethod
     def write_changes_to_file(cls, filename):
+
         with open(filename, "w") as class_file:
             for obj in cls.ALL_STUDENTS:
-                print(obj)
+
                 row = ",".join(obj.to_list())
-                print(row)
+
                 class_file.write(row + "\n")
             class_file.close()
 
@@ -106,6 +138,9 @@ class Attendance():
         for day_at_school in self.attendance_list:
             if day_at_school.date == date:
                 day_at_school.status = status
+                return
+
+        raise ValueError('There is no such date.')
 
     def __str__(self):
 
@@ -126,16 +161,35 @@ class Attendance():
         return Attendance()
 
 
-Student.loading_file('students.csv')
+class Submission():
+
+    def __init__(self,link,description, assignment,grades=[3,2]):
+        self.link = link
+        self.description = description
+        self.grades = grades
+        self.assignment = assignment
+        self.submissions_list = []
+
+
+    def add_submission_to_submissions_list(self):
+        self.submissions_list.append(Submission(self.link,self.description,self.assignment,self.grades))
+
+
+
+Student.loading_file('students2.csv')
+
+
+
+#Mike = Student('Mike', 'Beckingham', 22, 'Male','12345678912', 'DaveBeckingham','abc123')
+
+#Mike.list_of_attendance.checking_presence('2088/1/20',1)
+
+
+#Mike.submit_submission('www.asbc.com','Done','To_do_list')
 
 for student in Student.get_all():
-    student.list_of_attendance.checking_presence('2017/1/17',2)
-    student.list_of_attendance.checking_presence('2017/2/18',2)
-    student.list_of_attendance.checking_presence('2018/3/19', 1)
+     print(student)
 
-Dave = Student('Dave', 'Beckingham', 22, 'Male','12345678912', 'DaveBeckingham','abc123')
-
-Dave.list_of_attendance.checking_presence('2017/1/17',2)
 
 
 Student.write_changes_to_file('students2.csv')
