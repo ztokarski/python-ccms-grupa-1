8d18bfe9a5a0daf39e70b93cf50473435030692e
oskardz <oskardz1991@gmail.com>
2017-01-17 11:31:25 +0100

    skeleton

--- mentor.py
+++ mentor.py
@@ -1,6 +1,27 @@
-import Person from person.py
+import employee from employee.py
+import attendace from attendace.py
+import submissions from submissions.py
 
-class Mentor(Person):
 
-def __init__(self)
-pass
+class Mentor(Employee):
+
+
+def __init__(self,name, surname, age, gender, pesel, login, password, date_addeed, date_remove, status, class_attendance, grades, submissions):
+    Employee.__init__(name, surname, age, gender, pesel, login, password, date_addeed, date_remove, status)
+
+def create_mentor():
+    pass
+
+def make_assigment():
+    pass
+
+def grade_subbmission():
+    pass
+
+def check_attendance():
+    pass
+
+@classmethod
+
+def get_all():
+    pass
