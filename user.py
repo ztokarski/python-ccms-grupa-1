560271663ec99b14ad9fa6dd0a900cb24d84014a
Michal <r.pr.michal.doniec@gmail.com>
2017-01-17 11:20:09 +0100

    Implemented validate_password method, date_create attr in user class.

--- user.py
+++ user.py
@@ -1,33 +1,57 @@
-class User:
-    ALL_USERS = []
+import time
 
-    def __init__(self,name, surname, age, gender, pesel, login, password, date_added, date_removed=None, status = 'Active'):
+
+class User:
+    '''Class containing basic data pertaining to all users of the system.'''
+
+    ALL_USERS = []  #to store all instances of User class
+
+    def __init__(self,name, surname, age, gender, pesel, login, password, date_removed=None, status = 'Active'):
+        '''Initialize User object. '''
+
         self.name = name
         self.surname = surname
         self.age = age
         self.gender = gender
         self.pesel = pesel
         self.login = login
-        self.password = password
-        self.date_added = date_added
+        self._password = password
+        date_when_added = time.localtime()
+        self.date_added = '{}/{}/{}'.format(date_when_added[0], date_when_added[1], date_when_added[2])
         self.date_removed = date_removed
         self.status = status
         self.ALL_USERS.append(self)
 
+
+    def validate_password(self, given_password):
+        '''To validate password and provide access to the user account.'''
+
+        if given_password == self._password:
+            return True
+
+        else:
+            return False
+
+
     @classmethod
     def get_all(cls):
+        'Return all instances of User class.'
+
         return cls.ALL_USERS
 
 
 
 
-Adam = User('Adam', 'Mickiewicz', 100, 'Male', 192345567, 'Adam','Mickiewicz','02.02.2015')
+Adam = User('Adam', 'Mickiewicz', 100, 'Male', 192345567, 'Adam','Mickiewicz')
 
-
+print(Adam.date_added)
 print(User.ALL_USERS)
 
 print(User.get_all())
 
+print(Adam.validate_password('abc'))
+
+print(Adam.validate_password('Mickiewicz'))
 
 
 
