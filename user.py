3ad3d6a63dbd0c5c0f41601e959b773143174981
oskardz <oskardz1991@gmail.com>
2017-01-18 14:36:49 +0100

    stupid files added

--- user.py
+++ user.py
@@ -45,7 +45,7 @@ class User:
             count = 1
             for line in class_file:
                 print(line)
-                User(line[0],line[1],line[2], line[3],line[4],line[5],line[6],line[7],lined[8])
+                User(line[0],line[1],line[2], line[3],line[4],line[5],line[6],line[7],line[8])
 
 
 
