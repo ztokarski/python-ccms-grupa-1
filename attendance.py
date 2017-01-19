43f08050a633fc400610953be90808c1b7510c7a
Michal <r.pr.michal.doniec@gmail.com>
2017-01-19 13:18:30 +0100

    Changing int into str in student & attendance classes.

--- attendance.py
+++ attendance.py
@@ -2,7 +2,7 @@
 class Attendance():
     '''Attendance class containing data pertaining to all student attendance as well as each student attendance.'''
 
-    def __init__(self, date = ' ', status = 0):
+    def __init__(self, date = ' ', status = '0'):
         '''Method that initialize instance of Attendance class.'''
 
         self.date =  date
@@ -28,13 +28,13 @@ class Attendance():
         '''Method that overwrittes default __str__ method and changes student status from int into string and
         returns string with basic data from instance of attendance class.'''
 
-        if self.status == 2:
+        if self.status == '2':
             student_status = 'present'
 
-        elif self.status == 1:
+        elif self.status == '1':
             student_status = 'late'
 
-        elif self.status == 0:
+        elif self.status == '0':
             student_status = 'absent'
 
         return 'Student was {} at the day {}.'.format(student_status, self.date)
